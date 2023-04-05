817db81ff441c766e0a0309e94049f499d4f4f2c
Ashik <designwithashik@gmail.com>
2023-04-05 09:10:53 +0600

    Header Styles Added with Active Link

--- src/components/ActiveLink.jsx
+++ src/components/ActiveLink.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './ActiveLink.css'
 
+const style = {
+    padding: '7px 15px',
+    borderRadius: "11px",
+    color: 'white',
+    fontWeight: '500'
+}
 const ActiveLink = ({to, children}) => {
     return (
-        <div>
-            <NavLink to={to} className={isActive=>isActive? 'active':''}>{children}</NavLink>
-        </div>
+        <>
+            <NavLink style={style} to={to} className={({isActive})=>isActive?'active': 'inactive'}>{children}</NavLink>
+        </>
     );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ActiveLink from './ActiveLink';
+import { Container, Flex, Text } from '@chakra-ui/react';
 
 const Header = () => {
     return (
         <nav>
+            <Flex justifyContent='space-between' py='15px' backgroundColor='violet'>
+                <Container>
+                <Text fontSize='21px' fontWeight={500} color='white'>Food Resort</Text>
+                </Container>
+                <Container display='flex' gap='51px'> 
             <ActiveLink to='/'>Home</ActiveLink>
             <ActiveLink to='/menu'>Menu</ActiveLink>
             <ActiveLink to='/search'>Search</ActiveLink>
             <ActiveLink to='/cart'>Cart</ActiveLink>
+                </Container>
+            </Flex>
         </nav>
     );
 };
